adacf55deab2d44c95c09229d5919658652ad26f
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-06 13:48:50 -0400

    Complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    for (const element of array){
+        if ((args.includes(element) == false)){
+            newArray.push(element);
+        }
+    }
+    console.log(newArray);
+    return newArray;
 };
 
 // Do not edit below this line
