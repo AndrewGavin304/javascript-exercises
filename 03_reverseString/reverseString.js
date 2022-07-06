e165a71033b8a689befd77ec21a6119f0ddf8b49
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-06 12:52:17 -0400

    Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const strAsArray = str.split("");
+    const reverseArray = strAsArray.reverse();
+    return reverseArray.join("");
 };
 
 // Do not edit below this line
