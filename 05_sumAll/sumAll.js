9218750a639860aab81f6c0ea4d9500454407b8c
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-06 14:19:54 -0400

    Refactor

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,15 @@
 const sumAll = function(num1, num2) {
     if ((Number.isInteger(num1)) && (Number.isInteger(num2) && (num1 > 0) && (num2) > 0)){
-        if (num1<num2){
-            sum = 0
-            for (i=num1; i<=num2; i++){
-                sum += i;
-            }
-            return sum;
+        if (num1 > num2) {
+            const temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
-        else if (num2<num1){
-            sum = 0
-            for (i=num1; i>=num2; i--){
-                sum += i;
-            }
-            return sum;
+        let sum = 0
+        for (i=num1; i<=num2; i++){
+            sum += i;
         }
+        return sum;
     }
     else{
     return "ERROR";
