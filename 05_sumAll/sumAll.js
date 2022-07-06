a9dc5c927d3617ebd0fa10f45859caeaf85033aa
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-06 14:00:35 -0400

    Pass first two tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if ((Number.isInteger(num1) && Number.isInteger(num2))){
+        sum = 0
+        for (i=num1; i<=num2; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    else{
+        return "Error"
+    }
 };
 
 // Do not edit below this line
