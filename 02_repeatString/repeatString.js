35d7051fdbba8fec73c6ff4950a9f3a4c46ee21b
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-06 12:31:06 -0400

    Complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let returnString = ""
+    if (num < 0){
+        returnString = "ERROR"
+        return returnString;
+    }
+    for (let i = num; i >= 1; i--){
+        returnString += str;
+    }
+    return returnString;
 };
 
 // Do not edit below this line
