c19defb81d512046abc86576f7e42c52d8ccd14b
AndrewGavin304 <AndrewGavin304@gmail.com>
2022-07-09 13:02:41 -0400

    finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = (str) => {
+    const lowerStr = str.toLowerCase();
+    const charArray = lowerStr.split('');
+    const arrayNoSymbols = [];
+    for (const char of charArray){
+        if (char.match(/[a-z]/i)) {
+            arrayNoSymbols.push(char)
+        }
+    }
+    const reversedArrayNoSymbols = arrayNoSymbols.slice().reverse()
+    if (reversedArrayNoSymbols.toString() === arrayNoSymbols.toString()){
+        return true;
+    }
+    else{
+        return false;
+    }
 };
 
 // Do not edit below this line
